8ea72425ec0b15cb8dc3f3bc1047d9c2e6ff478c
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2022-12-25 12:51:46 +0330

    update: AuthorAdmin

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Author, Publisher, Book, BookLanguage
 
-admin.site.register(Author)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['author_first_name', 'author_first_name']
+    search_fields = ['author_first_name__istartswith',
+                     'author_last_name__istartswith']
+
+
 admin.site.register(Publisher)
 admin.site.register(BookLanguage)
 admin.site.register(Book)
