771c074c972d23bbde505306d28a2abc185b5075
rrfpCSIR <shes980@gmail.com>
2023-01-13 23:39:28 +0200

    book_quantity is updated when order is confirmed

--- orders/urls.py
+++ orders/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('all', views.OrdersView.as_view(), name='all_orders'),
     path('create_order/new', views.CreateOrder.as_view(success_url=reverse_lazy('orders:all_orders')), name='new_order'),
     path('all/<int:pk>/details', views.DetailOrder.as_view(), name='order_details'),
-
-    # path('all/<int:pk>/confirm_order/<int:order_status_id>', views.update_order_status, name='order_confirm'),
     path('all/<int:pk>/<int:order_status_id>', views.update_order_status, name='update_order_status'),
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -6,9 +6,44 @@ from .models import BookState, OrderStatus
 
 from orders.models import CustomerOrder
 
+CONFIRM_ORDER_STATUS_ID = 2
 
 def count_total_price(list_of_prices):
     return sum(list_of_prices)
+
+
+def update_order_status(request, *args, **kwargs):
+    """
+    when confirming order - validate quantity of books to order, update quantity and assign confirm status
+    if not valid - assign status ILLEGIBLE
+    :param request:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    ordered_books_id_set = CustomerOrder.objects.get(pk=kwargs['pk']).ordered_books.values_list('id', flat=True)
+    order_status_id = kwargs['order_status_id']
+    if order_status_id == CONFIRM_ORDER_STATUS_ID:
+        if validate_confirm_book_quantity(ordered_books_id_set):
+            update_book_quantity(ordered_books_id_set)
+        else:
+            order_status_id = OrderStatus.objects.get(status_value='ILLEGIBLE').pk
+    CustomerOrder.objects.filter(pk=kwargs['pk']).update(order_status_id=order_status_id)
+    customerorder = CustomerOrder.objects.get(pk=kwargs['pk'])
+    return render(request, 'orders/order_details.html', {'customerorder': customerorder})
+
+
+def validate_confirm_book_quantity(book_set): #1
+    for book in book_set:
+        if BookState.objects.get(book_id=book).book_quantity == 0:
+            return False
+    return True
+
+
+def update_book_quantity(book_set): #2
+    for book in book_set:
+        book_quantity = BookState.objects.get(book_id=book).book_quantity
+        BookState.objects.filter(book_id=book).update(book_quantity=book_quantity - 1)
 
 
 class OrdersView(ListView):
@@ -38,6 +73,11 @@ class DetailOrder(DetailView):
     model = CustomerOrder
     template_name = 'orders/order_details.html'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(DetailOrder, self).get_context_data(**kwargs)
+        context['book_quantity'] = BookState.objects.all()
+        return context
+
 
 class CreateOrder(CreateView):
     model = CustomerOrder
@@ -53,9 +93,3 @@ class CreateOrder(CreateView):
         form.instance.full_price = count_total_price(list_ordered_prices)
         super(CreateOrder, self).form_valid(form)
         return HttpResponseRedirect(self.get_success_url())
-
-
-def update_order_status(request, *args, **kwargs):
-    CustomerOrder.objects.filter(pk=kwargs['pk']).update(order_status_id=kwargs['order_status_id'])
-    customerorder = CustomerOrder.objects.get(pk=kwargs['pk'])
-    return render(request, 'orders/order_details.html', {'customerorder': customerorder})
