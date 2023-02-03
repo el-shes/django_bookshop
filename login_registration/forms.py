7010cd7f2cb4f70a1ebccf3d6eeb2326b0e1489a
rrfpCSIR <shes980@gmail.com>
2023-02-04 00:01:11 +0200

    user registration implemented

--- login_registration/forms.py
+++ login_registration/forms.py
@@ -1,11 +1,14 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+    email = forms.EmailField(max_length=254, help_text='Enter a valid email address')
 
     class Meta:
+        User = get_user_model()
         model = User
-        fields = ['username', 'email', 'first_name']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
