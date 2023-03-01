cc425146992a03ba2a9bf96f1ed78111a1d577ac
rrfpCSIR <shes980@gmail.com>
2023-03-01 23:39:04 +0200

    edit your profile info

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ app_name = 'users'
 
 
 urlpatterns = [
-    # path('create_user', views.CreateUserView.as_view(), name='user_create'),
     path('user/<int:pk>', views.ProfilePage.as_view(), name='profile_page'),
+    path('user/<int:pk>/edit_info', views.EditUserView.as_view(), name='edit_profile_page'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView
 from .models import User
 
 
@@ -6,6 +7,31 @@ class ProfilePage(DetailView):
     model = User
     template_name = 'users/profile_page_base.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(ProfilePage, self).get_context_data(**kwargs)
+        context['include_section'] = 'overview'
+        return context
 
-class CreateUserView:
-    pass
+
+class EditUserView(UpdateView):
+    model = User
+    template_name = 'users/profile_page_base.html'
+    fields = ['username', 'first_name', 'last_name', 'email']
+
+    def get_context_data(self, **kwargs):
+        context = super(EditUserView, self).get_context_data(**kwargs)
+        context['include_section'] = 'info_update'
+        return context
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('users:profile_page', kwargs={'pk': self.kwargs['pk']})
+
+
+class ChangeUserPassword(UpdateView):
+    model = User
+    template_name = 'users/profile_page_base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ChangeUserPassword, self).get_context_data(**kwargs)
+        context['include_section'] = 'change_password'
+        return context
